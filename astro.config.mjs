cf4e6d92c09b2b7de2d2f113fd78ad032a66949d
Peanut <truongdq.dev@gmail.com>
2025-01-14 20:28:10 +0700

    feat: change settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://truongnat.github.io',
+	site: 'https://truongnat.github.io/blog',
   	base: 'blog',
 	integrations: [mdx(), sitemap()],
 });
