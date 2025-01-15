1eddec2282c036abd8e4895acac70ac32b68150c
Peanut <truongdq.dev@gmail.com>
2025-01-15 18:26:08 +0700

    fix: change optional

--- src/utils/get-internal-link.ts
+++ src/utils/get-internal-link.ts
@@ -1,5 +1,5 @@
 import { DEFAULT_SITE_URL } from "../consts";
 
 export function getInternalLink(path?: string) {
-    return `${DEFAULT_SITE_URL}${path.startsWith('/') ? path : `/${path}`}`
+    return `${DEFAULT_SITE_URL}${path?.startsWith('/') ? path : `/${path}`}`
 }
