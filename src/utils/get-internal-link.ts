5ee8914eece1a9e1b93ff066abc504169f4a6f9c
Peanut <truongdq.dev@gmail.com>
2025-01-15 18:56:37 +0700

    feat: change base 2

--- src/utils/get-internal-link.ts
+++ src/utils/get-internal-link.ts
@@ -1,6 +1,5 @@
 import { DEFAULT_SITE_URL } from "../consts";
 
 export function getInternalLink(path?: string) {
-    const p = path ? path?.startsWith('/') ? path : `/${path}` : '/'
-    return `${DEFAULT_SITE_URL}${p}`
+    return  path ?  '/': path;
 }
