3645eeb824f542d9d85cbd4659d29065c44c05e1
Peanut <truongdq.dev@gmail.com>
2025-01-15 17:39:26 +0700

    feat: change base

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,5 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
 	site: 'https://truongnat.github.io',
 	integrations: [mdx(), sitemap()],
-	build: {
-		// Set base path to '/' to avoid the `/blog` folder in the deployed URL
-		base: '/',
-	  },
+	base: '/',
 });
